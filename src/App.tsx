05df6fb2159549b818aaf53c7bfd39e9cf1de747
Arun Murugan <arunmurugan200@gmail.com>
2021-04-06 13:37:02 +0530

    setup typed scss module

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Slider } from "./components/slider"
 
 function App() {
   return (
     <div>
+      <Slider maxValue={300} currentValue={10} />
     </div>
   );
 }
