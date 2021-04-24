8c5c402b7476127ed7a90879000faa948fed1a21
Arun Murugan <arunmurugan200@gmail.com>
2021-04-24 11:24:05 +0530

    Added husky githook to lint and format code on commiting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,4 @@
 module.exports = {
-    extends: ['airbnb'],
-    rules: {
-      'react/jsx-filename-extension': 'off',
-      //You can override any rules you want
-    },
-  };
+  extends: ['airbnb'],
+  rules: {},
+};
