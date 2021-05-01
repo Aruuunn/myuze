640e98e7dd0d5e32af9b72a464812536a0d200b0
Arun Murugan <arunmurugan200@gmail.com>
2021-05-01 14:23:09 +0530

    Added animation for music player page to slide from bottom to top

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,14 +1,18 @@
 import React, { ReactElement } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Route, Switch, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
 
 import { MusicPlayerPage, HomePage } from './pages';
 
 export function Routes(): ReactElement {
+  const location = useLocation();
   return (
-    <BrowserRouter>
-      <Route path="/play/:id" exact component={MusicPlayerPage} />
-      <Route path="/" exact component={HomePage} />
-    </BrowserRouter>
+    <AnimatePresence>
+      <Switch location={location} key={location.pathname}>
+        <Route path="/play/:id" exact component={MusicPlayerPage} />
+        <Route path="/" exact component={HomePage} />
+      </Switch>
+    </AnimatePresence>
   );
 }
 
