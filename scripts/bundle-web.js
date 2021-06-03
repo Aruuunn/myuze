faf6b78732c71e15c99b2e8621f6320275d6be19
Arun Murugan <arunmurugan200@gmail.com>
2021-06-03 21:19:39 +0530

    ci:  bundle all backend dependencies into a single file

--- scripts/bundle-web.js
+++ scripts/bundle-web.js
@@ -1,10 +1,10 @@
-const { renameSync, mkdirSync } = require('fs');
+const { renameSync } = require('fs');
 const { execSync } = require('child_process');
-
-mkdirSync('dist');
-
-renameSync('packages/web/build', 'packages/api/dist/public');
 
 execSync('yarn exec lerna run bundle --scope=@open-music-player/api');
 
-renameSync('packages/api/dist/@open-music-player/api', 'dist/main');
+renameSync('packages/api/bundle', 'dist');
+
+renameSync('packages/web/build', 'dist/public');
+
+
